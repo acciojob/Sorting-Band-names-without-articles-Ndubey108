9c932388d8c09001a08bc69fd708b29b7fc419e1
Nidhi Dubey <93912780+Ndubey108@users.noreply.github.com>
2023-05-28 15:01:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
+function removeArticle(bandName) {
+ return bandName.join(',').replace(/^(a |an |the )/i, '').split(',').sort();
+}
+let names=removeArticle(['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal']);
+let ulcontainer=document.querySelector('ul');
 
+names.map(name=>{
+	let li=document.createElement('li');
+	li.innerText=name;
+	ulcontainer.appendChild(li);
+})
